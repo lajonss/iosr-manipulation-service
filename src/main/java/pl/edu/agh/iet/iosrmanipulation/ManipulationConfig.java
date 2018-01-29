4343d847e8bc3581003537d87bd461a5584c9e7c
lajonss <gfs.b16@gmail.com>
2018-01-30 00:28:09 +0100

    static property fix

--- src/main/java/pl/edu/agh/iet/iosrmanipulation/ManipulationConfig.java
+++ src/main/java/pl/edu/agh/iet/iosrmanipulation/ManipulationConfig.java
@@ -1,14 +1,23 @@
 package pl.edu.agh.iet.iosrmanipulation;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import pl.edu.agh.iet.iosrmanipulation.manipulator.AdditiveManipulator;
 import pl.edu.agh.iet.iosrmanipulation.manipulator.Manipulator;
+import pl.edu.agh.iet.iosrmanipulation.manipulator.MultiplicativeManipulator;
 
 @Configuration
+@ConfigurationProperties(prefix = "manipulator")
 public class ManipulationConfig {
+    String operation;
+
     @Bean
     Manipulator getManipulator() {
-        return Manipulator.newManipulator();
+        if (operation.compareTo("ADD") == 0)
+            return new AdditiveManipulator();
+        else if (operation.compareTo("MULTIPLY") == 0)
+            return new MultiplicativeManipulator();
+        return new Manipulator();
     }
 }
--- src/main/java/pl/edu/agh/iet/iosrmanipulation/manipulator/Manipulator.java
+++ src/main/java/pl/edu/agh/iet/iosrmanipulation/manipulator/Manipulator.java
@@ -2,20 +2,8 @@ package pl.edu.agh.iet.iosrmanipulation.manipulator;
 
 import java.util.Random;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "manipulator")
 public class Manipulator {
-    private static String operation;
     protected Random random = new Random();
-
-    public static Manipulator newManipulator() {
-        if (operation.compareTo("ADD") == 0)
-            return new AdditiveManipulator();
-        else if (operation.compareTo("MULTIPLY") == 0)
-            return new MultiplicativeManipulator();
-        return new Manipulator();
-    }
 
     public double transform(double value) {
         return value + 1;
