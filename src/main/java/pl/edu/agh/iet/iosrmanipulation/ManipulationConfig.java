221bdb13c98029bb985fed7809980c5a33bb1f77
lajonss <gfs.b16@gmail.com>
2018-01-30 00:32:25 +0100

    another take on problem

--- src/main/java/pl/edu/agh/iet/iosrmanipulation/ManipulationConfig.java
+++ src/main/java/pl/edu/agh/iet/iosrmanipulation/ManipulationConfig.java
@@ -1,16 +1,17 @@
 package pl.edu.agh.iet.iosrmanipulation;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import pl.edu.agh.iet.iosrmanipulation.manipulator.AdditiveManipulator;
 import pl.edu.agh.iet.iosrmanipulation.manipulator.Manipulator;
 import pl.edu.agh.iet.iosrmanipulation.manipulator.MultiplicativeManipulator;
 
 @Configuration
-@ConfigurationProperties(prefix = "manipulator")
 public class ManipulationConfig {
-    String operation;
+    @Value("${manipulator.operation}")
+    private String operation;
 
     @Bean
     Manipulator getManipulator() {
